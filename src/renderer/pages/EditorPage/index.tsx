9f458cd78db245a49995a08e6fc0e4515eba161b
FredTsang <17154608+FredZeng@users.noreply.github.com>
2022-05-21 17:33:23 +0800

    feat: init EditorActions

--- src/renderer/pages/EditorPage/index.tsx
+++ src/renderer/pages/EditorPage/index.tsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import EditorActions from './EditorActions';
 
 import './index.less';
 
 const EditorPage: React.FC = () => {
   return (
     <div className="full-screen editor-root">
-      <div className="h-full flex-shrink-0 composite-bar">{/* TODO: */}</div>
+      <div className="h-full flex-shrink-0 composite-bar">
+        <EditorActions />
+      </div>
       <div className="h-full flex-shrink-0 explorer-folders-view">{/* TODO: */}</div>
       <div className="h-full flex-grow">{/* TODO: */}</div>
     </div>
